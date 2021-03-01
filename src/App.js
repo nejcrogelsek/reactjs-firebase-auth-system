fe6b257235f220ce8645039419c54c066ba5e1ff
Nejc <nejcrogelsek0@gmail.com>
2021-03-01 22:06:00 +0100

    code + deploy for auth system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import {
+  facebookProvider,
+  githubProvider,
+  googleProvider,
+} from "./config/authMethods";
+import socialMediaAuth from "./service/auth";
 
 function App() {
+  const handleOnClick = async (provider) => {
+    const res = await socialMediaAuth(provider);
+    console.log(res);
+  };
   return (
-    <div className="app">
-      helloo
+    <div className='app'>
+      <button onClick={() => handleOnClick(facebookProvider)}>Facebook</button>
+      <button onClick={() => handleOnClick(githubProvider)}>Github</button>
+      <button onClick={() => handleOnClick(googleProvider)}>Google</button>
     </div>
   );
 }
---
+++ src/config/firebase-config.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyAREsOCW14vQf6zUpaxh0h8YjcVvGmqOIg",
+  authDomain: "fir-login-methods.firebaseapp.com",
+  projectId: "fir-login-methods",
+  storageBucket: "fir-login-methods.appspot.com",
+  messagingSenderId: "274551887883",
+  appId: "1:274551887883:web:2d1367cef4892203dacf0e",
+});
+export default firebase;
