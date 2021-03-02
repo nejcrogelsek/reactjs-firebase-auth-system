643c3c8903a9aeb74edcfb3de608b63ba265a5a8
Nejc <nejcrogelsek0@gmail.com>
2021-03-02 23:31:42 +0100

    logout system

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import SocialButton from "./components/SocialButton";
 import {
   facebookProvider,
@@ -5,38 +6,87 @@ import {
   googleProvider,
 } from "./config/authMethods";
 import socialMediaAuth from "./service/auth";
+// Redux
+import { login, logout, selectUser } from "./features/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { auth } from "./config/firebase-config";
+import firebase from "./config/firebase-config";
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const [isUser, setIsUser] = useState(false);
+
+  useEffect(() => {
+    console.log(user);
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+        setIsUser(true);
+      } else {
+        dispatch(logout());
+        setIsUser(false);
+      }
+    });
+  }, [dispatch]);
+
   const handleOnClick = async (provider) => {
     const res = await socialMediaAuth(provider);
-    console.log(res);
   };
+
+  const signout = () => {
+    dispatch(logout());
+    setIsUser(false);
+    firebase.auth().signOut();
+  };
+
   return (
     <ul className='app'>
-      <SocialButton
-        name='Facebook'
-        provider={facebookProvider}
-        handleOnClick={handleOnClick}
-        buttonClass='facebook'
-        iconType='fab'
-        iconName='fa-facebook-f'
-      />
-      <SocialButton
-        name='Google'
-        provider={googleProvider}
-        handleOnClick={handleOnClick}
-        buttonClass='google'
-        iconType='fab'
-        iconName='fa-google-plus-g'
-      />
-      <SocialButton
-        name='Github'
-        provider={githubProvider}
-        handleOnClick={handleOnClick}
-        buttonClass='github'
-        iconType='fab'
-        iconName='fa-github'
-      />
+      {isUser ? (
+        <>
+          <p>Hello {user.displayName}</p>
+          <button className='logout' onClick={() => signout()}>
+            <div className='buttonContent'>
+              <i className='fas fa-sign-out-alt'></i>
+              <span>logout</span>
+            </div>
+          </button>
+        </>
+      ) : (
+        <>
+          <SocialButton
+            name='Facebook'
+            provider={facebookProvider}
+            handleOnClick={handleOnClick}
+            buttonClass='facebook'
+            iconType='fab'
+            iconName='fa-facebook-f'
+          />
+          <SocialButton
+            name='Google'
+            provider={googleProvider}
+            handleOnClick={handleOnClick}
+            buttonClass='google'
+            iconType='fab'
+            iconName='fa-google-plus-g'
+          />
+          <SocialButton
+            name='Github'
+            provider={githubProvider}
+            handleOnClick={handleOnClick}
+            buttonClass='github'
+            iconType='fab'
+            iconName='fa-github'
+          />
+        </>
+      )}
     </ul>
   );
 }
